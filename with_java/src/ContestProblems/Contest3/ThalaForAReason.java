7cae4453ef706841ef17d50dba38d2ecdfe19662
lalitupadhyay-dev <ulalit498@gmail.com>
2026-03-02 20:44:28 +0530

    Solved Thala For A Reason Problem

--- with_java/src/ContestProblems/Contest3/ThalaForAReason.java
+++ with_java/src/ContestProblems/Contest3/ThalaForAReason.java
@@ -2,13 +2,36 @@ package ContestProblems.Contest3;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class ThalaForAReason {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        int n = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
 
-        
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            StringTokenizer stz = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(stz.nextToken());
+        }
+
+        for (int i = 0; i < n; ++i) {
+            int ele = arr[i];
+            int sum = 0;
+            while(ele > 0) {
+                sum += ele % 10;
+                ele /= 10;
+            }
+
+            if (sum == 7) {
+                System.out.println("Thala for a reason");
+            } else {
+                System.out.println("Blocked for no reason");
+            }
+
+        }
 
     }
 }
