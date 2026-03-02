4654357baae3b7a9741eb81464b32d4099163eed
lalitupadhyay-dev <ulalit498@gmail.com>
2026-03-02 21:24:35 +0530

    Solved the problem Toggle Me

--- with_java/src/ContestProblems/Contest3/ToggleMe.java
+++ with_java/src/ContestProblems/Contest3/ToggleMe.java
@@ -1,4 +1,23 @@
 package ContestProblems.Contest3;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ToggleMe {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        sb.append(br.readLine());
+
+        for (int i = 0; i < sb.length(); ++i) {
+            if ((int)sb.charAt(i) >= 65 && (int)sb.charAt(i) <= 90) {
+                sb.setCharAt(i, (char)((int)sb.charAt(i) + 32));
+            } else {
+                sb.setCharAt(i, (char)((int)sb.charAt(i) - 32));
+            }
+        }
+
+        System.out.println(sb);
+
+    }
 }
