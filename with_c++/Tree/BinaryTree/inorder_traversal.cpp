77d42e7432f51cc0cba117749108f4fe70060150
Lalit Upadhyay <ulalit498@gmail.com>
2026-07-18 21:14:53 +0530

    Solved few tree questions

---
+++ with_c++/Tree/BinaryTree/inorder_traversal.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+TreeNode* buildTree(const vector<string>& nodes) {
+    if (nodes.empty() || nodes[0] == "null") return nullptr;
+
+    TreeNode* root = new TreeNode(stoi(nodes[0]));
+    queue<TreeNode*> q;
+    q.push(root);
+
+    int i = 1;
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* cur = q.front();
+        q.pop();
+
+        if (nodes[i] != "null") {
+            cur->left = new TreeNode(stoi(nodes[i]));
+            q.push(cur->left);
+        }
+        i++;
+
+        if (i < nodes.size() && nodes[i] != "null") {
+            cur->right = new TreeNode(stoi(nodes[i]));
+            q.push(cur->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+void helper (TreeNode* root, vector<int>& ans) {
+    if (root == NULL) return;
+    helper(root->left, ans);
+    ans.push_back(root->val);
+    helper(root->right, ans);
+}
+
+// ================= STUDENT FUNCTION =================
+vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> ans;
+    helper(root, ans);
+    return ans;
+}
+// ====================================================
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<string> nodes(n);
+    for (int i = 0; i < n; i++) cin >> nodes[i];
+
+    TreeNode* root = buildTree(nodes);
+
+    vector<int> result = inorderTraversal(root);
+
+    for (int i = 0; i < result.size(); i++) {
+        if (i) cout << " ";
+        cout << result[i];
+    }
+    cout << '\n';
+}
